cfe50d0c33d97d2540992ee0f7eab740b182dcf6
vanshjangir <vanshjangir0001@gmail.com>
2023-05-26 19:51:44 +0530

    parsing bugs fixed

--- src/model.py
+++ src/model.py
@@ -5,59 +5,100 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
-# importing dataset 
-dataset = pd.read_csv('../datasets/merged.csv')
-dataset = dataset.dropna().reset_index()
-dataset.text = dataset.text.apply(lambda x: x.split())
-dataset = dataset[['text', 'label']]
 
-#spilltings into training and testing
-Xtrain, Xtest, ytrain, ytest = train_test_split(dataset['text'], dataset['label'], test_size=0.2)
+def vectorize(traindoc):
+    '''
+    takes a corpus and returns its word2vec model and vector form
+    and also saves the model for future use
+    '''
 
-#creating word2vec model
-w2v_model = gensim.models.Word2Vec(
-    window = 10,
-    min_count = 2,
-)
+    w2v_model = gensim.models.Word2Vec(
+        window = 10,
+        min_count = 2,
+    )
 
-#building vocabulary and testing
-w2v_model.build_vocab(Xtrain)
-w2v_model.train(Xtrain, total_examples = w2v_model.corpus_count, epochs = w2v_model.epochs)
+    w2v_model.build_vocab(traindoc)
+    w2v_model.train(traindoc, total_examples = w2v_model.corpus_count, epochs = w2v_model.epochs)
 
-#setting the all vectors to same lenght 
-words = set(w2v_model.wv.index_to_key )
-Xtrain_vect = np.array([np.array([w2v_model.wv[i] for i in ls if i in words], dtype=object)
-                         for ls in Xtrain], dtype=object)
-Xtest_vect = np.array([np.array([w2v_model.wv[i] for i in ls if i in words], dtype=object)
-                         for ls in Xtest], dtype=object)
+    words = set(w2v_model.wv.index_to_key )
+    traindoc_vect = np.array([np.array([w2v_model.wv[i] for i in ls if i in words], dtype=object)
+                         for ls in traindoc], dtype=object)
 
+    traindoc_vect_avg = []
+    for v in traindoc_vect:
+        if v.size:
+            traindoc_vect_avg.append(v.mean(axis=0))
+        else:
+            traindoc_vect_avg.append(np.zeros(100, dtype=float))
 
-Xtrain_vect_avg = []
-for v in Xtrain_vect:
-    if v.size:
-        Xtrain_vect_avg.append(v.mean(axis=0))
-    else:
-        Xtrain_vect_avg.append(np.zeros(100, dtype=float))
+    filename = 'w2v_model.sav'
+    joblib.dump(w2v_model, filename)
+
+    print("word2vec done!!")
+    return w2v_model,traindoc_vect_avg 
+
+def makevector(w2v_model, doc):
+    '''
+    takes a word2vec model and a corpus and returns its vector form 
+    '''
+
+    words = set(w2v_model.wv.index_to_key )
+    doc_vect = np.array([np.array([w2v_model.wv[i] for i in ls if i in words], dtype=object)
+                         for ls in doc], dtype=object)
         
-Xtest_vect_avg = []
-for v in Xtest_vect:
-    if v.size:
-        Xtest_vect_avg.append(v.mean(axis=0))
-    else:
-        Xtest_vect_avg.append(np.zeros(100, dtype=float))
+    doc_vect_avg = []
+    for v in doc_vect:
+        if v.size:
+            doc_vect_avg.append(v.mean(axis=0))
+        else:
+            doc_vect_avg.append(np.zeros(100, dtype=float))
+    return doc_vect_avg
 
 
 
+def SvmModel(doc_vect_avg, target):
+    '''
+    takes a corpus in vector form and returns a SVM trained model 
+    '''
 
-#creating a SVM model 
-model = SVC()
+    model = SVC()
+    print("training model...")
+    model.fit(doc_vect_avg, target)
+    print("model training done!!")
+    return model 
 
-model.fit(Xtrain_vect_avg, ytrain)
 
-model.score(Xtest_vect_avg, ytest)
+def savemodel(model):
+    '''
+    saves the model locally
+    '''
+
+    filename = 'finalized_model.sav'
+    joblib.dump(model, filename)
+
+def accuracy(model,test,target):
+    return model.score(test,target)
 
 
-#Saving the model for future use
-filename = 'finalized_model.sav'
-joblib.dump(model, filename)
+def main():
+    # importing dataset 
+    dataset = pd.read_csv('../datasets/merged.csv')
+    dataset = dataset.dropna().reset_index()
+    dataset.text = dataset.text.apply(lambda x: x.split())
+    dataset = dataset[['text', 'label']]
+
+    #spilltings into training and testing
+    Xtrain, Xtest, ytrain, ytest = train_test_split(dataset['text'], dataset['label'], test_size=0.2)
+
+    w2v_model,Xtrain_vect_avg = vectorize(Xtrain)
+    Xtest_vect_avg = makevector(w2v_model,Xtest)
+    model = SvmModel(Xtrain_vect_avg, ytrain)
+    print(accuracy(model,Xtest_vect_avg, ytest))
+    savemodel(model)
+
+if __name__ == "__main__":
+    main()
+    
+
+
 
