29e000b2c78d6fa06a853703e83a4b29f4d3accd
vanshjangir <vanshjangir0001@gmail.com>
2023-06-05 23:49:01 +0530

    Application part starting now

--- src/main.py
+++ src/main.py
@@ -1,14 +1,91 @@
 import model 
+import youtube
 import joblib
+import gensim
+import tkinter as tk
+import customtkinter as ctk 
+import configparser
+
+loadmodel = joblib.load("finalized_model.sav")
+w2v_model = joblib.load("w2v_model.sav")
+
+
+def textfromfile():
+    '''
+    takes a .txt file and returns a list of all the sentences
+    '''
+    pass 
+
+def textfrominput():
+    '''
+    prompts user to give input in a testbox and returns a list of sentences
+    '''
+    pass 
+
+def youtubecomments(root):
+    '''
+    returns list of comments on a youtube video 
+    '''
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    api_key = config['youtube']['api_key']
+    video_id = ""
+   
+    LinkEntry = ctk.CTkEntry(root, placeholder_text = "link")
+    LinkEntry.place(relx = 0.5, rely = 0.6, anchor = "center")
+    
+    link = "https://www.youtube.com/watch?v=6Zs8Kp8YULA&t=1s"
+    
+    for i in range(len(link)):
+        if link[i] == "=": 
+            video_id = link[i+1:i+12]
+            break 
+
+    comments = youtube.fetch_video_comments(video_id, api_key)
+    print(len(comments))
+    result(root,comments)
+
+
+def result(root,text):
+    '''
+    Opens the entry box to type link and show the results
+    by calling appropriate functions
+    '''
+    test = []
+    target = []
+    for each in text:
+        test.append(gensim.utils.simple_preprocess(each, deacc=False, min_len=2, max_len=50))
+        target.append(1)
+
+    test = model.makevector(w2v_model, test)
+    score = loadmodel.predict(test)
+
+    positive= 0 
+    negative = 0 
+    for i in score:
+        if i == 1:
+            negative += 1
+        else:
+            positive += 1
+
+    print(positive,negative)
 
 def main():
-    loadmodel = joblib.load("finalized_model.sav")
-    w2v_model = joblib.load("w2v_model.sav")
+    root = ctk.CTk()
+    root.geometry('800x500')
+    root.title('Hate Speech Detection')
 
-    sentence = input("enter a sentence\n")
-    sen_vec = model.makevector(w2v_model,[sentence.split()])
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
 
-    print(model.accuracy(loadmodel,sen_vec,[1]))
+    head = ctk.CTkLabel(root, text = "HateSpeechDetector", fg_color = "transparent")
+    head.place(relx = 0.5, rely = 0.2, anchor = "center")
+    
+    BtnYoutube = ctk.CTkButton(root, text = "youtube", state = "normal", command = lambda: youtubecomments(root))
+    BtnYoutube.place(relx = 0.5, rely = 0.5, anchor = "center")
+
+    root.mainloop()
 
 
 if __name__ == "__main__":
---
+++ src/youtube.py
@@ -0,0 +1,40 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+def fetch_video_comments(video_id, api_key):
+    # Create a YouTube API client
+    youtube = build('youtube', 'v3', developerKey = api_key)
+
+    try:
+        # Get video comments using the video's ID
+        comments = []
+        nextPageToken = None
+
+        while True:
+            response = youtube.commentThreads().list(
+                part='snippet',
+                videoId=video_id,
+                pageToken=nextPageToken,
+                maxResults=100
+            ).execute()
+
+            # Iterate through each comment thread
+            for item in response['items']:
+                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                comments.append(comment)
+
+            # Check if there are more comments
+            nextPageToken = response.get('nextPageToken')
+            if not nextPageToken:
+                break
+
+        return comments
+
+    except HttpError as e:
+#        print('An HTTP error occurred:')
+#        print(e)
+        return []
+
+
+
+
