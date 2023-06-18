68e733170a9e3d2acc86cf1dcc8ebf8360e65f71
vansh <vanshjangir0001@gmail.com>
2023-06-18 15:22:19 +0530

    file selection bug fixed, html style on one page only

--- src/web.py
+++ src/web.py
@@ -8,19 +8,22 @@ def index():
     if request.method == "POST":
         textdata = request.form['text']
         linkdata = request.form['youtube link']
-        filedata = request.form['file']
-        file = open(filedata, "r")
-        filetextdata = file.read()
 
-        print(len(filetextdata))
+        try:
+            filedata = request.form['file']
+            file = open(filedata, "r")
+            filetextdata = file.read()
+
+        except:
+            filetextdata = ""
 
         final = ""
         if len(textdata) != 0:
-            final += bridge.userinput(text = textdata)
+            final += str(bridge.userinput(text = textdata))
         if len(linkdata) != 0:
-            final += bridge.userinput(link = linkdata)
+            final += str(bridge.userinput(link = linkdata))
         if len(filetextdata) != 0:
-            final += bridge.userinput(text = filetextdata)
+            final += str(bridge.userinput(text = filetextdata))
 
         def generate():
             for line in final.split("\n\n"):
